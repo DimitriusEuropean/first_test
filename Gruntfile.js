6ef75d3ce17c478f4d995fbbc51ef9710f1f528b
Dima Sivash <sixtime@rambler.ru>
2016-07-19 17:37:06 +0300

    Added new Cruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,24 @@
 // Обязательная обёртка
 module.exports = function(grunt) {
 
+
   grunt.initConfig({
+
+    watch: {
+      sass: {
+        files: ['sass/*.scss'],
+        tasks: ['sass', 'cssmin'],
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : 'sass/style.scss'
+        }
+      }
+    },
+
     concat: {
       main: {
         src: [
@@ -16,15 +33,29 @@ module.exports = function(grunt) {
           'js/production.min.js': '<%= concat.main.dest %>'
         }
       }
+    },
+    cssmin: {
+      my_target: {
+        files: [{
+          expand:true,
+          cwd: 'css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css/',
+          ext: '.min.css'
+        }]
+      }
     }
   });
 
   // Загрузка плагинов, установленных с помощью npm install
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Задача по умолчанию
-  grunt.registerTask('default', ['concat','uglify']);
+  grunt.registerTask('default', ['watch']);
   //grunt.registerTask('debug', ['uglify']);
 
 };
