41d00ffe77add3398556601c46ffdd130f688ff8
Dima Sivash <sixtime@rambler.ru>
2016-08-03 17:52:54 +0300

    Well done BEM

--- js/common.js
+++ js/common.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
     $('.hexagon').hover(function(){
     	var myClass = $(this).attr('data-class');
-        var removeClass = $('.description .description_up div').attr('class');
-        $('.description .description_up div').removeClass(removeClass);
-        $('.description .description_up div').addClass(myClass + ' foto');
-        var myDesc = $(this).find('.desc').text();
-        $('.description .description_down .text').text(myDesc);
-        var myTitle = $(this).find('.name_tool').text();
-        $('.description .description_up .name').text(myTitle);
+
+        var removeClass = $('.description-up div').attr('class');
+
+        
+        $('.description-up div').removeClass(removeClass);
+        $('.description-up div').addClass('description-up__foto ' + 'description-up__foto_' + myClass);
+        var myDesc = $(this).find('.desc').text();        
+        $('.description-down__text').text(myDesc);
+        var myTitle = $(this).find('.name-tool').text();
+        $('.description-up__name').text(myTitle);
+        $('.js-hexagon').removeClass('js-hexagon-selected');
+        $(this).addClass('js-hexagon-selected');
+
     });
 
 });
